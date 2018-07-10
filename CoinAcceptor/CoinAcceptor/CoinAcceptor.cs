166f4abec9f830ab13869fabc299c17cddd3a020
MayteePalamee <m.palami@hotmail.com>
2018-07-10 16:34:32 +0700

    Test commit 2

---
+++ CoinAcceptor/CoinAcceptor/CoinAcceptor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinAcceptor{
+
+    public class CoinAcceptor {
+       
+        /*provides event data*/
+        public delegate void eventhandle(object sender, String status);
+        /*event delegate.*/
+        public event eventhandle events;
+
+        private void onConnect(String command)
+        {
+            eventhandle raiseEvent = events;
+            Status status = new Status();
+            /* try
+             {
+
+             }
+             catch(Exception ex)
+             {*/
+            //status.Fault = ex.Message;
+            status.Fault = "Connection Fault";
+            raiseEvent(this, status.Fault);
+            //}
+        } 
+
+        private void onDisconncet(String command)
+        {
+            eventhandle raiseEvent = events;
+            Status status = new Status();
+            /* try
+             {
+
+             }
+             catch(Exception ex)
+             {*/
+            //status.Fault = ex.Message;
+            status.Fault = "Disconnect Fault";
+            raiseEvent(this, status.Fault);
+            //}
+        }
+    }
+}
---
+++ CoinAcceptor/CoinAcceptor/Status.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinAcceptor
+{
+    public class Status
+    {
+        private String connect;
+        private String disconnect;
+        private String fault;
+        private String unavailable;
+
+        public String Connect
+        {
+            get { return connect; }
+            set { connect = value; }
+        }
+        public String Disconnect
+        {
+            get { return disconnect; }
+            set { disconnect = value; }
+        }
+        public String Fault
+        {
+            get { return fault; }
+            set { fault = value; }
+        }
+        public String Unavailable
+        {
+            get { return unavailable; }
+            set { unavailable = value; }
+        }
+    }
+}
